706ace7d24c3c1db54ec2cff5bef775f0c229baf
rahulp0 <rahul.prksh1@gmail.com>
2017-06-15 23:48:49 +0530

    Add files via upload

---
+++ 1.game/another.js
@@ -0,0 +1,11 @@
+
+.button{
+        background-color:rgb(0,0,0);
+        font-family:"Arial";
+        font-size:19px;
+        padding:190px 23px;
+
+    }
+
+.button2{background-color:rgb(0,0,255);}
+.button3.{background-color:rgb(200,0,0);}
---
+++ 1.game/somejs.js
@@ -0,0 +1,5 @@
+function mars()
+{
+     document.location.href='https://chordify.net/';
+}
+document.getElementById('hover').addEventListener('mouseover',mars);
