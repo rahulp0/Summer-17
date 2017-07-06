b94c9bb5c77ff5a1789917a338b26bdcbe398f03
rahulp0 <rahul.prksh1@gmail.com>
2017-07-06 12:18:25 +0530

    Add files via upload

--- projectEuler/projectEuler16.js
+++ projectEuler/projectEuler16.js
@@ -1,4 +1,42 @@
-'use strict'
+
+var number = [1],
+    sum = 0;
+
+for(var i = 0; i < 1000; i++)
+{
+    var overflow = 0,
+        count = number.length + 1;
+
+    for(var j = 0; j < count; j++)
+    {
+        var digit = number[j] || 0;
+
+        digit = 2 * digit + overflow;
+
+        if(digit > 9)
+        {
+            digit -= 10;
+            overflow = 1;
+        }
+        else
+        {
+            overflow = 0;
+        }
+
+        number[j] = digit;
+    }
+}
+
+for(var i = 0; i < 1000; i++)
+{
+    sum += number[i];
+}
+
+console.log(sum);
+console.log(number);
+/*'use strict'
+
+
 var giv = Math.pow(2,1000);
 var strToArr=[];
 
@@ -8,9 +46,11 @@ var res=2;
 var final;
 var i=1
 while(i<1001){
-    (res*=2);
-    final=eval(res);
+     eval(res*=2);
+    //final=eval(res);
 	i++;
 }
 
 console.log(final);
+
+*/
